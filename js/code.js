fe266695629575e90891983d1403c6ee1b3fffca
Marc <marcortizgonzalez2002@gmail.com>
2021-10-08 19:51:11 +0200

    Subida JS

--- js/code.js
+++ js/code.js
@@ -1,3 +1,10 @@
 function calculo(params) {
-    alert("hola");
+    //alert("hola");
+    let totalCuenta = document.getElementById('total').value;
+    alert(totalCuenta);
+    let servicio = document.getElementById('servicio').value;
+    alert(servicio);
+    let personas = document.getElementById('personas').value;
+    alert(personas);
+    console.log('total: ' + totalCuenta + ' servicio: ' + servicio + ' personas: ' + personas);
 }
